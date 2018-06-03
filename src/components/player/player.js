3edf0d516f0653a9516eab9c56e6b6f8f8a8fb03
tiantian2000 <1592770232@qq.com>
2018-06-03 17:29:07 +0800

    update

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -241,7 +241,7 @@ class Player extends Component {
                 lyricElS = lyricElS.getElementsByTagName('li');
                 let lyricEl = lyricElS[this.state.lineNo]
                 let scrollObj = this.refs.scroll.getScrollObj()
-                scrollObj.scrollToElement(lyricEl,3000)
+                scrollObj.scrollToElement(lyricEl,4000)
                 this.setState({
                     lineNum: 0
                 })
